7d609efe211f8ca6628d3d6d99d03a8ad39a65a7
Jakob Ladekær <jml@kapacity.dk>
2021-02-18 21:22:14 +0100

    adding git clone to command.sh

--- commands.sh
+++ commands.sh
@@ -1,1 +1,3 @@
-az webapp up -sku F1 -n udacity-ci-cd-project --resource-group udacity_assign2
+git clone git@github.com:jakobmladekaer/udacity_CI_CD_pipeline.git
+cd udacity_CI_CD_pipeline
+az webapp up --sku F1 -n udacity-ci-cd-project --resource-group udacity_assign2
